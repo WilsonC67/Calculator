ea4927414b92a821a2bc321697ce899fc914237a
WilsonC67 <wilson.chen@quinnipiac.edu>
2025-02-05 20:20:16 -0500

    Added a function that prompts the user for two whole numbers, and then it prints the calculated sum.

--- Calculator.java
+++ Calculator.java
@@ -3,13 +3,12 @@ public class Calculator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		
 		System.out.println("Addition: Please enter two whole numbers.");
 		int firstNumber = scanner.nextInt();
 		int secondNumber = scanner.nextInt();
 		
 		int total = firstNumber + secondNumber;
-		System.out.println("The sum of the two provided numbers is " + total);
+		System.out.println(String.format("%d + %d = %d", firstNumber, secondNumber, total));
 		
 		scanner.close();
 	}
