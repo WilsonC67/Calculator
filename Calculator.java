9ad99f9867309b777c0a2de3c6ffaae5d9325151
WilsonC67 <wilson.chen@quinnipiac.edu>
2025-02-05 21:33:06 -0500

    Added multiplication and division functions; The code now uses a switch statement instead of if-else statements. Also, there is a try-catch statement to potentially catch an InputMismatchException, but I'm not sure if I implemented it correctly, or if it's job is already covered by the default case in the switch segment. Overall, it is more descriptive.

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,97 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class Calculator {
+	
 	public static void main(String[] args) {
+		
 		Scanner scanner = new Scanner(System.in);
 		
 		int firstNumber;
 		int secondNumber;
-		int total;
+		int sum;
+		int difference;
+		int product;
+		int quotient;
+		String operationLetter;
+
+			
+		System.out.println("Would you like to execute addition, subtraction, "
+				+ "multiplication, or division?\n"
+				+ "Please indicate by typing any operation's corresponding character.\nA/S/M/D\n");
 		
-		System.out.println("Would you like to execute addition or subtraction?\nA/S");
-		String operationLetter = scanner.nextLine();
-		System.out.println(operationLetter);
 		
+		operationLetter = scanner.nextLine().toUpperCase();
+	
+		
+		try {
+			switch(operationLetter) {
+				
+			case "A":
+					
+					System.out.println("You have chosen: Addition");
+					System.out.println("Please enter two whole numbers for addition.");
+					
+					firstNumber = scanner.nextInt();
+					secondNumber = scanner.nextInt();
+					sum = firstNumber + secondNumber;
+					
+					System.out.println(String.format("%d + %d = %d", firstNumber, secondNumber, sum));
+					
+					break;
+	
+				case "S":
+						
+					System.out.println("You have chosen: Subtraction");
+					System.out.println("Please enter two whole numbers for subtraction.\n"
+							+ "Note that the first number will be the minuend and the second will thus be "
+							+ "the subtrahend.");
+					
+					firstNumber = scanner.nextInt();
+					secondNumber = scanner.nextInt();
+					difference = firstNumber - secondNumber;
+					
+					System.out.println(String.format("%d - %d = %d", firstNumber, secondNumber, difference));
+					break;
+					
+				case "M":
+					
+					System.out.println("You have chosen: Multiplication");
+					System.out.println("Please enter two whole numbers for multiplication.\n");
+					firstNumber = scanner.nextInt();
+					secondNumber = scanner.nextInt();
+					product = firstNumber * secondNumber;
+					System.out.println(String.format("%d * %d = %d", firstNumber, secondNumber, product));
+					break;
+					
+				case "D":
+					
+					System.out.println("You have chosen: Division");
+					System.out.println("Please enter two whole numbers for division.\n"
+							+ "Note that the first number will be the dividend and the second"
+							+ "number will be the divisor.\n"
+							+ "Also, all quotients will be rounded to be integers.");
+					firstNumber = scanner.nextInt();
+					secondNumber = scanner.nextInt();
+					quotient = firstNumber / secondNumber;
+					System.out.println(String.format("%d / %d = %d", firstNumber, secondNumber, quotient));
+					break;
+				
+				default: 
+					System.out.println("That is not a valid operator.");
+			}
+		} 
+		
+		catch (InputMismatchException e){
+			System.err.println("This is not a valid input.");
+			e.printStackTrace();
+		}
+				
+		scanner.close();
+		
+		// old version:
+		
+		/*
 		if (operationLetter.equals("A")) {
 			System.out.println("Please enter two whole numbers for addition.");
 			
@@ -30,12 +111,9 @@ public class Calculator {
 			total = firstNumber - secondNumber;
 			
 			System.out.println(String.format("%d - %d = %d", firstNumber, secondNumber, total));
-		} 
-		/*
-		else {
+		} else {
 			System.out.println("Please enter a valid letter to indicate the operation.");
-		}
 		*/
-		scanner.close();	
+		
 	}
 }
