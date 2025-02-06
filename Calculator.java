5a99d447638f3c8ff2968ea0ff8331dbab5a4d5f
WilsonC67 <wilson.chen@quinnipiac.edu>
2025-02-05 20:47:58 -0500

    Now allows the user to choose either addition or subtraction as their operation, and then prints out their chosen calculation.

--- Calculator.java
+++ Calculator.java
@@ -3,13 +3,39 @@ public class Calculator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Addition: Please enter two whole numbers.");
-		int firstNumber = scanner.nextInt();
-		int secondNumber = scanner.nextInt();
+		int firstNumber;
+		int secondNumber;
+		int total;
 		
-		int total = firstNumber + secondNumber;
-		System.out.println(String.format("%d + %d = %d", firstNumber, secondNumber, total));
+		System.out.println("Would you like to execute addition or subtraction?\nA/S");
+		String operationLetter = scanner.nextLine();
+		System.out.println(operationLetter);
 		
-		scanner.close();
+		if (operationLetter.equals("A")) {
+			System.out.println("Please enter two whole numbers for addition.");
+			
+			firstNumber = scanner.nextInt();
+			secondNumber = scanner.nextInt();
+			total = firstNumber + secondNumber;
+			
+			System.out.println(String.format("%d + %d = %d", firstNumber, secondNumber, total));
+
+		} else if (operationLetter.equals("S")) {
+			System.out.println("Please enter two whole numbers for subtraction.\n"
+					+ "Note that the first number will be the minuend and the second will thus be "
+					+ "the subtrahend.");
+			
+			firstNumber = scanner.nextInt();
+			secondNumber = scanner.nextInt();
+			total = firstNumber - secondNumber;
+			
+			System.out.println(String.format("%d - %d = %d", firstNumber, secondNumber, total));
+		} 
+		/*
+		else {
+			System.out.println("Please enter a valid letter to indicate the operation.");
+		}
+		*/
+		scanner.close();	
 	}
 }
